a24bd9d128f5186e14dff953d008b4be5932ec42
PruthvirajAuti-Git <autipruthviraj53@gmail.com>
2026-04-10 06:15:50 +0000

    challenge_2

---
+++ Challenge_2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+
+   enum comp{GOOGLE,FACEBOOK,XEROX,YAHOO,EBAY,MICROSOFT};
+   enum comp g=GOOGLE;
+   enum comp x=XEROX;
+   enum comp e=EBAY;
+   printf("%d",x);
+   printf("\n");
+   printf("%d\n",g);
+   printf("%d",e);
+
+
+
+
+
+
+}
