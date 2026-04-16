043b44e0533a3bef843a9ac913580686ed8d04d2
PruthvirajAuti-Git <autipruthviraj53@gmail.com>
2026-04-16 06:31:39 +0000

    Challenge 5

---
+++ Challenge_5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{   
+    printf("Variable of type char occupy %zd\n",sizeof(char));
+    printf("int:%zd\n",sizeof(int));
+     printf("long:%zd\n",sizeof(long));
+       printf("long long:%zd\n",sizeof(long long));
+      printf("float:%zd\n",sizeof(float));
+       printf("double :%zd\n",sizeof(double));
+         printf("long double :%zd\n",sizeof(long double));
+}
