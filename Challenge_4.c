e20456154cbe33a415168de6dda2d851eca48f9f
PruthvirajAuti-Git <autipruthviraj53@gmail.com>
2026-04-16 06:12:40 +0000

    challenge 4

---
+++ Challenge_4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{     
+   float min,day,year;
+   printf("Enter Min:");
+   scanf("%f",&min);
+   day=min/1440;
+   year=min/525960;
+   printf("Enter min are :%f\n",min);
+   printf("In Year;%f\n",year);
+   printf("In days :%f\n",day);
+   
+
+
+
+
+
+}
